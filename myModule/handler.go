f4d4bd7a3e81c2403d8bdbcfcb26d6557cd68858
goya5858 <hokuzaki1203@gmail.com>
2022-01-07 00:21:53 +0900

    add age

--- myModule/handler.go
+++ myModule/handler.go
@@ -14,10 +14,11 @@ func executeFunction() {
 
 type MyEvent struct {
 	Name string `json:name`
+	Age  int
 }
 
 func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
-	fmt.Println(event)
+	fmt.Println("event:", event)
 	return fmt.Sprintf("Hello %s!", event.Name), nil
 }
 
