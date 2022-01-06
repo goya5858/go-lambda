683f49dc1c403588c8ea3abb2d4208a03e26533d
goya5858 <hokuzaki1203@gmail.com>
2022-01-07 00:15:32 +0900

    change name

--- myModule/handler.go
+++ myModule/handler.go
@@ -16,8 +16,9 @@ type MyEvent struct {
 	Name string `json:name`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
+	fmt.Println(event)
+	return fmt.Sprintf("Hello %s!", event.Name), nil
 }
 
 func main() {
