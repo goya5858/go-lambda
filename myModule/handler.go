1299257a254439002971ed4ae001f59f55bae1e3
goya5858 <hokuzaki1203@gmail.com>
2022-01-07 00:11:18 +0900

    add handler

--- myModule/handler.go
+++ myModule/handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"myModule/myPackage" //myModule(モジュール名)のmyPackage(パッケージ名)を使用する
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -10,7 +12,16 @@ func executeFunction() {
 	myPackage.SayHello() //
 }
 
+type MyEvent struct {
+	Name string `json:name`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
 func main() {
-	lambda.Start(executeFunction)
+	lambda.Start(HandleRequest)
+	//lambda.Start(executeFunction)
 	//executeFunction()
 }
