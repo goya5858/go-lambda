d30a27533d58dc78eb5ec0bb73f188c23e14eb3f
goya5858 <hokuzaki1203@gmail.com>
2022-01-07 16:50:47 +0900

    modified

--- myModule/converter/requestConverter.go
+++ myModule/converter/requestConverter.go
@@ -7,8 +7,8 @@ import (
 
 // Request
 type Request struct {
-	line_count string `json:"line_count"` // 特に意味のないデータ　複数を扱うための仮のもの
-	text       string `json:"text"`       //base64でエンコードされたデータ
+	line_count string // 特に意味のないデータ　複数を扱うための仮のもの
+	text       string //base64でエンコードされたデータ
 }
 
 type requestConverterImpl struct{}
@@ -23,7 +23,7 @@ func (impl requestConverterImpl) Exec(requestBody string) (*Request, error) {
 	fmt.Println("jsonBytes:", jsonBytes)
 	fmt.Println("Before req:", req)
 
-	if unmarshalErr := json.Unmarshal(jsonBytes, req); unmarshalErr != nil {
+	if unmarshalErr := json.Unmarshal(jsonBytes, &req); unmarshalErr != nil {
 		return nil, unmarshalErr
 	}
 	fmt.Println("After req:", req)
